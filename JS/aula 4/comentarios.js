fe57fcb4c9d7ff9ceb872c826c92fecd48ae17e5
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-28 12:40:24 -0300

    11. Comentários de código

---
+++ JS/aula 4/comentarios.js
@@ -0,0 +1,16 @@
+// Assim é escrito um comentário
+console.log('Olá Mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+
+/**
+ * Assim é um bloco de codigo
+ * para escrever em varias linhas
+ */
+
+/* 
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// Este comentário também vai ser ignorado no processamento
