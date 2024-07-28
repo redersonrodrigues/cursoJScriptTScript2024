02f9510e41c46da46553cb1491bed58c9727aa75
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-28 11:57:35 -0300

    1. Instalando NodeJS, VSCode e Chrome no Windows 2. Instalando NodeJS, VSCode e Chrome no Linux (Ubuntu 18.04) 3. Não se esqueça de salvar seus arquivos antes de executar 4. Entenda como o curso está estruturado 5. Perguntas e Respostas - Como funciona? 6. Seção de HTML e CSS (Novo) 7. Console.log - A primeira coisa que você vai ver em JS 8. Execícios 9. Repositório do curso no Github 10. Me ajude a produzir conteúdo gratuito

---
+++ JS/aula 3/exercicio.js
@@ -0,0 +1,3 @@
+
+// O 10 é um numero literal e o restante string
+console.log('meu nome é "REDERSON".Estou aprendendo JavaScript as', 10,' da manhã');
---
+++ JS/aula 3/index.js
@@ -0,0 +1,10 @@
+//Strings
+console.log('Luiz Otávio');
+console.log("Luiz Otávio");
+console.log(`Luiz Otávio`); // template screen
+console.log("Luiz 'Otávio'");
+console.log('Luiz "Otávio"');
+console.log(`'Luiz' "Otávio"`);
+// Números
+console.log(123456);
+console.log(35, 15.85, 'Luiz Otávio de novo');
