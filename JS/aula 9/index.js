eacf9bde9de7ae53bbb15b912cb6238a34303b01
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-28 21:18:36 -0300

    17. Código do exercício solucionado 18. Let vs Var - Primeira diferença

---
+++ JS/aula 9/index.js
@@ -0,0 +1,7 @@
+// let nome = 'Luiz';
+var nome = 'Luiz';
+var nome = 'Otavio';// com var aceita multi declaração
+console.log(nome);
+
+
+// nome = 'Luiz'; // NUNCA FAÇA ASSIM -> é uma declaração aceita mas é errado
