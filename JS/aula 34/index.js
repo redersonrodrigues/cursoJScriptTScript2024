c0ec53bd6529d55c73fa21c3f9c4843ed2c4dc62
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-08-09 18:54:21 -0300

    52. For - Clássico - Estrutura de repetição

---
+++ JS/aula 34/index.js
@@ -0,0 +1,36 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+
+// i = index
+// for (let i =0;i <= 5 ; ++i){
+//     console.log(`Linha ${i}`);
+    
+// }
+
+// for (let i = 400;i <= 500 ; i += 10){
+//         console.log(`Linha ${i}`);
+// }
+
+// for (let i = -100;i <= 500 ; i += 10){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 500;i >= 400 ; i -= 10){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 0;i <= 10 ; i++){
+//     const par = i % 2 === 0 ? `par`: `impar`;
+//     console.log(i,par);
+// }
+
+const frutas = ['Maça','Pera','Uva','Luiz','Otávio','João','Miranda'];
+
+for (let i = 0;i < frutas.length ; i++){
+    console.log(`Indice ${i} = `,frutas[i]);
+}
