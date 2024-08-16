2fc817bfd6976e441f086ef9147f48a28ba9287b
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-08-15 21:41:05 -0300

    103. Getters e Setters

---
+++ JS/seção_7_Javascript_Classes_Programação_Orientada_a_Objetos_POO/aula 81/index.js
@@ -0,0 +1,63 @@
+const _velocidade = Symbol('velocidade');
+class Carro {
+    constructor(nome) {
+        this.nome = nome;
+        this[_velocidade] = 0;
+    }
+    set velocidade(valor) {
+        console.log('SETTER');
+        
+        if (typeof valor !== 'number') return;
+        if (valor >= 100 || valor <= 0) return;
+        this[_velocidade] = valor;
+    }
+
+    get velocidade() {
+        console.log('GETTER');
+        
+        return this[_velocidade];
+    }
+
+    acelerar() {
+        if (this[_velocidade] >= 100) return;
+        this[_velocidade]++
+    }
+    freiar() {
+        if (this[_velocidade] <= 0) return;
+        this[_velocidade]--;
+    }
+}
+
+const c1 = new Carro('Fusca');
+
+// for (let i = 0; i <= 200; i++) {
+//     c1.acelerar();
+// }
+
+// c1.velocidade = 99;
+console.log(c1.velocidade);
+
+// exemplo 2
+class Pessoa   {
+    constructor(nome,sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+    
+    get nomeCompleto(){
+        return `${this.nome} ${this.sobrenome}`;
+    }
+
+    set nomeCompleto(valor) {
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome =  valor.join(' ');
+    }
+}
+
+const p1 = new Pessoa('Luiz','Miranda');
+p1.nomeCompleto = 'Luiz Miranda de Oliveira';
+console.log(p1);
+console.log();
+console.log(p1.nome);
+console.log(p1.sobrenome);
