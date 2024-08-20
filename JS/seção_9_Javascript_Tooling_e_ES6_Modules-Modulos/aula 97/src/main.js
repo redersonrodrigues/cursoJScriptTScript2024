b3d5a51ffac8fa10d44a28ca76995267682cf9ef
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-08-19 22:15:39 -0300

    122. Core-js e regenerator-runtime

---
+++ JS/seção_9_Javascript_Tooling_e_ES6_Modules-Modulos/aula 97/src/main.js
@@ -0,0 +1,9 @@
+/*** Sempre que precisar que minha aplicação rode em navegadores mais antigos coloco as duas proximas importações */
+import 'core-js/stable'
+import 'regenerator-runtime/runtime';
+
+import  executa from "./modules/promises";
+
+import './assets/css/style.css';
+
+executa();
